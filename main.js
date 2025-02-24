66e7bb9902c68444a2ea7148bd8290d6ead53e45
Mateus Marques <mateusmarques2001@usp.br>
2025-02-24 20:20:55 -0300

    There is only one undesired feature: pressing '2++' displays 4

--- main.js
+++ main.js
@@ -1,18 +1,111 @@
-function add(a, b) {
-  return a + b;
+function round(x) {
+  const factor = 10 ** 6;
+  return Math.round(factor * x) / factor;
 }
 
-function operator(a, op, b) {
+// notice that Number('') === 0 is JavaScript
+function operate(a, op, b) {
+  a = Number(a);
+  b = Number(b);
   switch (op) {
     case "+":
-      return a + b;
+      return String(round(a + b));
     case "*":
-      return a * b;
+      return String(round(a * b));
     case "/":
-      return a / b;
+      return String(round(a / b));
     case "-":
-      return a - b;
+      return String(round(a - b));
     default:
       console.error("operator: Not an operator");
   }
 }
+
+function populateDisplay(button) {
+  displayText(a);
+  if (firstOperand) {
+    if (button === '.' || !isNaN(button)) { // number
+      a += button;
+    }
+    else {  // symbol
+      firstOperand = false;
+      op = button;
+    }
+    displayText(a);
+  }
+  else {
+    if (button === '.' || !isNaN(button)) { // number
+      b += button;
+      displayText(b);
+    }
+    else {  // symbol
+      if (b !== '') { // in case there is no "b", we just update "op"
+        a = operate(a, op, b);
+        displayText(a);
+        b = '';
+      }
+      op = button;
+    }
+  }
+}
+
+function equal() {
+  if (!firstOperand) {
+    a = operate(a, op, b);
+    displayText(a);
+    b = '';
+    firstOperand = true;
+  }
+}
+
+function displayText(text) {
+  if (text === '')  // Number('') === 0 in JavaScript
+    display.textContent = '0';
+  else
+    display.textContent = text;
+}
+
+function clearDisplay() {
+  display.textContent = '0';
+  firstOperand = true;
+  a = '';
+  op = '';
+  b = '';
+}
+
+function invert() {
+  let string = display.textContent;
+  let ch = string.at(0);
+  if (ch === '-') {
+    displayText(string.replace('-', ''));
+    if (firstOperand)
+      a = a.replace('-', '');
+    else
+      b = a.replace('-', '');
+  }
+  else {
+    displayText(display.textContent = '-' + string);
+    if (firstOperand)
+      a = '-' + a;
+    else
+      b = '-' + b;
+  }
+}
+
+function percentage() {
+  if (firstOperand) {
+    a = a / 100;
+    displayText(a);
+  }
+  else {
+    b = b / 100;
+    displayText(b);
+  }
+}
+
+let firstOperand = true;
+
+const display = document.querySelector("#display");
+let a = '';
+let op = '';
+let b = '';
